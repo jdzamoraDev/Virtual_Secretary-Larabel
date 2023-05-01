c31eb69cbfbd1ab392b6d27ebc9c609c796edd26
zamor3x <josedzamoraj02@gmail.com>
2023-04-30 22:58:43 -0600

    update view store 1.2 and welcome 1.1

--- resources/views/store.blade.php
+++ resources/views/store.blade.php
@@ -7,58 +7,59 @@
 			name="viewport"
 			content="width=device-width, initial-scale=1.0"
 		/>
-		<title>Tienda</title>
-		<link rel="stylesheet" href="../css/store.css" />
+		<title>Store</title>
+		<link rel="stylesheet" href="css/store.css" />
 	</head>
 	<body>
 		<header>
-			<h1>Tienda</h1>
+			<h1>Saprissa Official Store</h1>
 
 			<div class="container-icon">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke-width="1.5"
-					stroke="currentColor"
-					class="icon-cart"
-				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
-					/>
-				</svg>
-				<div class="count-products">
-					<span id="contador-productos">0</span>
+				<div class="container-cart-icon">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="icon-cart"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+						/>
+					</svg>
+					<div class="count-products">
+						<span id="contador-productos">0</span>
+					</div>
 				</div>
 
 				<div class="container-cart-products hidden-cart">
-					<div class="cart-product">
-						<div class="info-cart-product">
-                            <span class="cantidad-producto-carrito">1</span>
-                            <p class="titulo-producto-carrito">Zapatos Nike</p>
-                            <span class="precio-producto-carrito">$80</span>
-                        </div>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke-width="1.5"
-							stroke="currentColor"
-							class="icon-close"
-						>
-							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								d="M6 18L18 6M6 6l12 12"
-							/>
-						</svg>
+					<div class="row-product hidden">
+						<div class="cart-product">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke-width="1.5"
+								stroke="currentColor"
+								class="icon-close"
+							>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M6 18L18 6M6 6l12 12"
+								/>
+							</svg>
+						</div>
 					</div>
-                    <div class="cart-total">
-                        <h3>Total:</h3>
-                        <span class="total-pagar">$200</span>
-                    </div>
+
+					<div class="cart-total hidden">
+						<h3>Total:</h3>
+						<span class="total-pagar">$200</span>
+					</div>
+					<p class="cart-empty">El carrito está vacío</p>
 				</div>
 			</div>
 		</header>
@@ -71,9 +72,9 @@
 					/>
 				</figure>
 				<div class="info-product">
-					<h2>Nueva Camiseta Kappa Ed. Jugador Mujer Manga Corta - Morada 2023 Regular Fit</h2>
+					<h2>New Kappa Ed. Player Women's Short Sleeve Jersey - Purple 2023 Regular Fit</h2>
 					<p class="price">$80</p>
-					<button>Añadir al carrito</button>
+					<button class="btn-add-cart">Add to cart</button>
 				</div>
 			</div>
 			<div class="item">
@@ -84,9 +85,9 @@
 					/>
 				</figure>
 				<div class="info-product">
-					<h2>Nueva Camiseta Kappa Ed. Jugador Hombre Manga Corta - Morada 2023 Regular Fit</h2>
+					<h2>New Kappa Ed. Player Men's Short Sleeve Shirt - Purple 2023 Regular Fit</h2>
 					<p class="price">$80</p>
-					<button>Añadir al carrito</button>
+					<button class="btn-add-cart">Add to cart</button>
 				</div>
 			</div>
 			<div class="item">
@@ -97,9 +98,9 @@
 					/>
 				</figure>
 				<div class="info-product">
-					<h2>Nueva Camiseta Kappa Ed. Jugador Mujer Manga Corta - Blanca 2023 Regular Fit</h2>
+					<h2>New Kappa Ed. Player Women's Short Sleeve Jersey - White 2023 Regular Fit</h2>
 					<p class="price">$80</p>
-					<button>Añadir al carrito</button>
+					<button class="btn-add-cart">Add to cart</button>
 				</div>
 			</div>
 			<div class="item">
@@ -110,9 +111,9 @@
 					/>
 				</figure>
 				<div class="info-product">
-					<h2>Nueva Camiseta Kappa Ed. Jugador Hombre Manga Corta - Blanca 2023 Regular Fit</h2>
+					<h2>New Kappa Ed. Player Men's Short Sleeve Jersey - White 2023 Regular Fit</h2>
 					<p class="price">$80</p>
-					<button>Añadir al carrito</button>
+					<button class="btn-add-cart">Add to cart</button>
 				</div>
 			</div>
 			<div class="item">
@@ -123,26 +124,26 @@
 					/>
 				</figure>
 				<div class="info-product">
-					<h2>SP Sueter Para Frío Kappa 2021 - Negro</h2>
+					<h2>SP Kappa Cold Weather Sweater 2021 - Black</h2>
 					<p class="price">$120</p>
-					<button>Añadir al carrito</button>
+					<button class="btn-add-cart">Add to cart</button>
 				</div>
 			</div>
             <div class="item">
 				<figure>
 					<img
-						src="https://cdn.shopify.com/s/files/1/0376/8329/8444/products/FOTO-PRODUCTO_4_0383dec6-f1ac-4305-934e-70f283765e28_600x.png?v=1633019909"
+						src="https://cdn.shopify.com/s/files/1/0376/8329/8444/products/FOTO-PRODUCTO_4_0383dec6-f1ac-4305-934e-70f283765e28_400x.png?v=1633019909"
 						alt="producto"
 					/>
 				</figure>
 				<div class="info-product">
-					<h2>SP ¡Campeón hay uno solo! Camiseta Campeón #36 Hombre - Negro</h2>
-					<p class="price">$11</p>
-					<button>Añadir al carrito</button>
+					<h2>SP There is only one champion! Men's Champion #36 Jersey - Black</h2>
+					<p class="price">$14</p>
+					<button class="btn-add-cart">Add to cart</button>
 				</div>
 			</div>
 		</div>
 
-        <script src="../js/store.js"></script>
+		<script src="js/store.js"></script>
 	</body>
 </html>
