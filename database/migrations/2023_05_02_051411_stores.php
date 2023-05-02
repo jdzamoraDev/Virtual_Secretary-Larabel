06560eff3191269fd01dbf2ad21d2c00c96284ec
ervingpinell <ervingpinell@gmail.com>
2023-05-01 23:29:53 -0600

    teest

--- database/migrations/2023_05_02_051411_stores.php
+++ database/migrations/2023_05_02_051411_stores.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('store', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->text('article');
             $table->integer('price');
